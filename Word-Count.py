54ecdaedd5957f4d68221af28d8b1d6ca8258aae
Peter Doyle <peter@Peters-MBP.hsd1.co.comcast.net>
2016-06-27 13:41:57 -0600

    added a filter on the Word-Count exercise to remove unwanted words based on a list of these words

--- Word-Count.py
+++ Word-Count.py
@@ -4,6 +4,8 @@ from pyspark import SparkConf, SparkContext
 def normalizeWords(text):
     return re.compile(r'\W+', re.UNICODE).split(text.lower())
 
+bad_word_list = ['', 's', 't']
+
 conf = SparkConf().setMaster("local").setAppName("WordCount")
 sc = SparkContext(conf = conf)
 
@@ -13,32 +15,26 @@ input = sc.textFile("/vagrant/Book.txt")
 #but let's clean the text up a bit and filter out special characters and consider upper and lowercase to be the same thing
 words = input.flatMap(normalizeWords)
 # results = words.collect()
-# for result in results:
-#    print result
 
 # #python function
 # #wordCounts = words.countByValue()
 
 #take a spark approach...
 #convert each word to a key/value pair with a value of 1
-wordCountsMap = words.map(lambda x: (x,1))
+wordCountsMap = words.map(lambda x: (x,1)).filter(lambda (x, y): x not in bad_word_list)
 # results = wordCountsMap.collect()
-# for result in results:
-#    print result
 
 
 #count words with reduceByKey so reduceByKey will build a Set of each work and count the 1s!
 wordCountsReduced = wordCountsMap.reduceByKey(lambda x, y: x + y)
-results = wordCountsReduced.collect()
-# for result in results:
-#    print result
+# results = wordCountsReduced.collect()
 
 wordCountsSorted = wordCountsReduced.map(lambda (x,y): (y,x)).sortByKey()
 results = wordCountsSorted.collect()
 
 for result in results:
-    # print result
-    count = str(result[0])
-    word = result[1].encode('ascii', 'ignore')
-    if (word):
-        print word + ":\t\t" + count
+    print result
+    # count = str(result[0])
+    # word = result[1].encode('ascii', 'ignore')
+    # if (word):
+    #     print word + ":\t\t" + count
